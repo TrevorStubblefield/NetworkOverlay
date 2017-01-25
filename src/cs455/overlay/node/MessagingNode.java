80d74594775f8706fa14b8941a033f851d632067
Trevor Stubblefield <trevor.stubblefield@hpe.com>
2017-01-25 14:12:13 -0700

    Some Basic Code

--- src/cs455/overlay/node/MessagingNode.java
+++ src/cs455/overlay/node/MessagingNode.java
@@ -1,8 +1,27 @@
 package cs455.overlay.node;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * Created by stubblet on 1/25/2017.
  */
 public class MessagingNode {
+    public static void main (String[] args){
+        try {
+            Socket socket = new Socket("16.78.182.242", 1234);
 
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("random text");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String text = input.readLine();
+            System.out.println(text);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/cs455/overlay/node/Registry.java
+++ src/cs455/overlay/node/Registry.java
@@ -1,8 +1,33 @@
 package cs455.overlay.node;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by stubblet on 1/25/2017.
  */
 public class Registry {
-    
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+
+            while(true){
+                String text = "No Message";
+                Socket socket = serverSocket.accept();
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                text = input.readLine();
+                System.out.println(text);
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println("response");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
