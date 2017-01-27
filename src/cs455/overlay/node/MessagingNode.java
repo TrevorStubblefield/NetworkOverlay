ca411e44717d4d7e2b209bb39bcead293c0f8358
Trevor Stubblefield <trevor.stubblefield@hpe.com>
2017-01-27 15:31:22 -0700

    1/27: Basic Messaging Node and Registry stuff

--- src/cs455/overlay/node/MessagingNode.java
+++ src/cs455/overlay/node/MessagingNode.java
@@ -3,25 +3,74 @@ package cs455.overlay.node;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
-/**
- * Created by stubblet on 1/25/2017.
- */
 public class MessagingNode {
     public static void main (String[] args){
+
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+        new MessagingNodeServer(args[0],Integer.parseInt(args[1]),queue).start();
+        new MessagingNodeCommands(queue).start();
+    }
+}
+
+class MessagingNodeServer extends Thread{
+
+    ServerSocket serverSocket;
+    Socket socket;
+    private BlockingQueue<String> queue;
+
+    MessagingNodeServer(String registryAddress, int registryPort, BlockingQueue<String> queue){
+
+        this.queue = queue;
         try {
-            Socket socket = new Socket("16.78.182.242", 1234);
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            String ipAddress = inetAddress.getHostAddress();
+            String hostName = inetAddress.getHostName();
+            this.serverSocket = new ServerSocket(0);
+            int port = serverSocket.getLocalPort();
 
+            //Tells registry ready to register.
+            this.socket = new Socket(registryAddress, registryPort);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println("random text");
+            out.println("Message Type (int): REGISTER_REQUEST\n" + ipAddress + "\n" + port);
 
+            //receives message back
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String text = input.readLine();
             System.out.println(text);
         }
-        catch (Exception e){
-            System.out.println(e.getMessage());
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
+
+class MessagingNodeCommands extends Thread{
+    private BlockingQueue<String> queue;
+
+    MessagingNodeCommands(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+
+    @Override
+    public void run(){
+
+        String command;
+
+        do {
+            Scanner cmdScanner = new Scanner(System.in);
+            command = cmdScanner.next();
+            System.out.println(command);
+
+        }while(!command.equals("exit"));
+    }
+}
+
+
--- src/cs455/overlay/node/Registry.java
+++ src/cs455/overlay/node/Registry.java
@@ -5,29 +5,114 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
-/**
- * Created by stubblet on 1/25/2017.
- */
 public class Registry {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+        new RegistryServerCommands(queue).start();
+        new RegistryServer(Integer.parseInt(args[0]), queue).start();
+    }
+}
+
+class RegistryServer extends Thread{
+
+    ServerSocket serverSocket;
+    Socket socket;
+    private int port;
+    private BlockingQueue<String> queue;
+
+    RegistryServer(int port, BlockingQueue<String> queue){
+        this.port = port;
+        this.queue = queue;
+    }
+
+    //TODO: Create data structure for nodes.
+    //TODO: Actually register the nodes and do the checking.
+    public void registerNode(String message){
+        System.out.println(message);
         try {
-            ServerSocket serverSocket = new ServerSocket(1234);
+            queue.put(message);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("response");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //TODO: All of this method.
+    public void deregisterNode(){}
+
+
+    @Override
+    public void run(){
+        try{
+            serverSocket = new ServerSocket(port);
 
             while(true){
-                String text = "No Message";
-                Socket socket = serverSocket.accept();
+
+                socket = serverSocket.accept();
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                text = input.readLine();
-                System.out.println(text);
+                String requestLine = input.readLine();
 
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println("response");
+                if(requestLine.contains("Message Type (int): REGISTER_REQUEST")){
+                    String request = requestLine + "\n" + input.readLine() + "\n" + input.readLine();
+                    registerNode(request);
+                }
+
+                else if(requestLine.contains("Message Type (int): DEREGISTER_REQUEST")){
+                    deregisterNode();
+                }
             }
-
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+//TODO: GET ACTUAL COMMANDS WORKING.
+class RegistryServerCommands extends Thread{
+
+    private BlockingQueue<String> queue;
+
+    RegistryServerCommands(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+
+    @Override
+    public void run(){
+
+        String command;
+
+        do {
+            Scanner cmdScanner = new Scanner(System.in);
+            command = cmdScanner.nextLine();
+            try {
+
+                if (command.equals("list-messaging nodes")) {
+
+                    while(!queue.isEmpty())
+                        System.out.println("I GOT IT " + queue.take());
+
+                } else if (command.contains("list-weights")) {
+
+                } else if (command.contains("setup-overlay")) {
+
+                } else if (command.contains("send-overlay-link-weights")) {
+
+                } else if (command.contains("start")) {
+                    int numberOfRounds = Integer.parseInt(command.substring(command.indexOf(" ")));
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }while(!command.equals("exit"));
     }
 }
